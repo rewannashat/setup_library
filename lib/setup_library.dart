0328d040b04d2a6031ca457c77283737ff069d6b
rewannashat <mrrwan55@gmail.com>
2025-03-13 04:40:45 +0200

    Add setup file

---
+++ lib/setup_library.dart
@@ -0,0 +1,23 @@
+// lib/setup_library.dart
+
+// Domain
+export 'domian/bloc_observer.dart';
+export 'domian/end-point.dart';
+export 'domian/local/sharedPref.dart';
+
+// Presentation
+export 'presentation/resources/colors-manager.dart';
+export 'presentation/resources/constants/custom-button-constant.dart';
+export 'presentation/resources/constants/custom-drawer-constant.dart';
+export 'presentation/resources/constants/custom-staticwidget.dart';
+export 'presentation/resources/constants/custom-text-constant.dart';
+export 'presentation/resources/constants/custom-textclick-constant.dart';
+export 'presentation/resources/constants/custom-textfield-constant.dart';
+export 'presentation/resources/font-manager.dart';
+export 'presentation/resources/routes-manager.dart';
+export 'presentation/resources/styles-manager.dart';
+export 'presentation/resources/theme-manager.dart';
+export 'presentation/resources/values_manager.dart';
+
+// Main
+export 'main.dart';
